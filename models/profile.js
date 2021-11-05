8484e424020df51a66234cdec3a5bc144cac2ff8
MrJanHorak <janny_b_goode@hotmail.com>
2021-11-05 17:16:25 -0400

    set up Google OAuth

--- models/profile.js
+++ models/profile.js
@@ -3,6 +3,9 @@ import mongoose from 'mongoose'
 const profileSchema = new mongoose.Schema({
   name: String,
   avatar: String,
+  // recipes: ,
+  // mealplans: ,
+  likeLevel: Number,
 }, {
   timestamps: true
 })
