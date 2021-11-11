1d76edf2b8761e9af851bcbccb3e1eaa65829f97
MrJanHorak <janny_b_goode@hotmail.com>
2021-11-11 15:30:54 -0500

    style add recipe view

--- public/js/recipes/addIngredient.js
+++ public/js/recipes/addIngredient.js
@@ -1,19 +1,16 @@
 function addFields(){
 
   let container = document.getElementById("container");
-  // container.appendChild(document.createTextNode("ingredient"));
-  // Create an <input> element, set its type and name attributes
-  // let input1 = document.createElement("input")
-  // let input2 = document.createElement("input")
-  let input3 = document.createElement("input")
-  // input1.type = "number"
-  // input2.type = "select"
-  input3.type = "text";
-  // input1.name = "amount"
-  // input2.name = "measurement"
-  input3.name = "ingredients";
-  // container.appendChild(input1);
-  // container.appendChild(input2);
-  container.appendChild(input3);
+
+  let input = document.createElement("input")
+
+  input.type = "text";
+
+  input.name = "ingredients";
+
+  input.size = "57"
+
+  container.appendChild(document.createElement("br"));
+  container.appendChild(input);
   container.appendChild(document.createElement("br"));
   }
