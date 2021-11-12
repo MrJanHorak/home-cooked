9ebb70e7557e85190f9276f7b0d45267a3aa9db2
MrJanHorak <janny_b_goode@hotmail.com>
2021-11-11 21:50:48 -0500

    style profile show view

--- server.js
+++ server.js
@@ -1,47 +1,47 @@
-import 'dotenv/config.js'
-import express from 'express'
-import path from 'path'
-import { fileURLToPath } from 'url'
-import createError from 'http-errors'
-import session from 'express-session'
-import logger from 'morgan'
-import methodOverride from 'method-override'
-import passport from 'passport'
-import { passUserToView } from './middleware/middleware.js'
+import "dotenv/config.js";
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import createError from "http-errors";
+import session from "express-session";
+import logger from "morgan";
+import methodOverride from "method-override";
+import passport from "passport";
+import { passUserToView } from "./middleware/middleware.js";
 
 // connect to MongoDB with mongoose
-import('./config/database.js')
+import("./config/database.js");
 
 // load passport
-import('./config/passport.js')
+import("./config/passport.js");
 
 // require routes
-import { router as indexRouter } from './routes/index.js'
-import { router as authRouter } from './routes/auth.js'
-import { router as mealplanRouter } from './routes/mealplans.js'
-import { router as recipeRouter } from './routes/recipes.js'
-import { router as profileRouter } from './routes/profiles.js'
+import { router as indexRouter } from "./routes/index.js";
+import { router as authRouter } from "./routes/auth.js";
+import { router as mealplanRouter } from "./routes/mealplans.js";
+import { router as recipeRouter } from "./routes/recipes.js";
+import { router as profileRouter } from "./routes/profiles.js";
 
 // create the express app
-const app = express()
+const app = express();
 
 // view engine setup
 app.set(
-  'views',
-  path.join(path.dirname(fileURLToPath(import.meta.url)), 'views')
-)
-app.set('view engine', 'ejs')
+  "views",
+  path.join(path.dirname(fileURLToPath(import.meta.url)), "views")
+);
+app.set("view engine", "ejs");
 
 // middleware
-app.use(methodOverride('_method'))
-app.use(logger('dev'))
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride("_method"));
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(
   express.static(
-    path.join(path.dirname(fileURLToPath(import.meta.url)), 'public')
+    path.join(path.dirname(fileURLToPath(import.meta.url)), "public")
   )
-)
+);
 
 // session middleware
 app.use(
@@ -50,40 +50,40 @@ app.use(
     resave: false,
     saveUninitialized: false,
     cookie: {
-      sameSite: 'lax',
-    }
+      sameSite: "lax",
+    },
   })
-)
+);
 
 // passport middleware
-app.use(passport.initialize())
-app.use(passport.session())
+app.use(passport.initialize());
+app.use(passport.session());
 
 //custom middleware
-app.use(passUserToView)
+app.use(passUserToView);
 
 // router middleware
-app.use('/', indexRouter)
-app.use('/auth', authRouter)
-app.use('/mealplans', mealplanRouter)
-app.use('/recipes', recipeRouter)
-app.use('/profiles', profileRouter)
+app.use("/", indexRouter);
+app.use("/auth", authRouter);
+app.use("/mealplans", mealplanRouter);
+app.use("/recipes", recipeRouter);
+app.use("/profiles", profileRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next(createError(404))
-})
+  next(createError(404));
+});
 
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message
-  res.locals.error = req.app.get('env') === 'development' ? err : {}
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
-  res.status(err.status || 500)
-  res.render('error', {
-    title: `🎊 ${err.status || 500} Error`
-  })
-})
+  res.status(err.status || 500);
+  res.render("error", {
+    title: `🎊 ${err.status || 500} Error`,
+  });
+});
 
-export { app }
+export { app };
