007ea2490791d256832aa1f0fcc33c66220dec16
MrJanHorak <janny_b_goode@hotmail.com>
2021-11-11 20:45:56 -0500

    Style profile indew view

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -1,55 +1,50 @@
-import { Profile } from "../models/profile.js"
-import { Recipe } from "../models/recipe.js"
-import { Mealplan } from "../models/mealplan.js"
+import { Profile } from "../models/profile.js";
+import { Recipe } from "../models/recipe.js";
+import { Mealplan } from "../models/mealplan.js";
 
 function index(req, res) {
   Profile.find({})
-  .then(profiles => {
-    Recipe.find({})
-    .then(recipes =>{
-      console.log("RECIPES ",recipes)
-      res.render("profiles/index", {
-        title: "Profiles",
-        profiles,
-        recipes
-      })
+    .then((profiles) => {
+      Recipe.find({}).then((recipes) => {
+        Mealplan.find({}).then((mealplans) => {
+          res.render("profiles/index", {
+            title: "Profiles",
+            profiles,
+            recipes,
+            mealplans,
+          });
+        });
+      });
     })
-  })
-  .catch(err => {
-    console.log(err)
-    res.redirect("/profiles")
-  })
+    .catch((err) => {
+      console.log(err);
+      res.redirect("/profiles");
+    });
 }
 
 function show(req, res) {
   Profile.findById(req.params.id)
-  .then((profile) => {
-    Recipe.find({owner: req.params.id})
-    .then(recipes => {
-      Mealplan.find({owner: req.params.id})
-      .then(mealplans =>{
-        Profile.findById(req.user.profile._id)
-        .then(self => {
-          const isSelf = self._id.equals(profile._id)
-          res.render("profiles/show", {
-            title: `${profile.name}'s profile`,
-            mealplans,
-            profile,
-            self,
-            isSelf,
-            recipes
-          })
-        })
-      })
-      })
+    .then((profile) => {
+      Recipe.find({ owner: req.params.id }).then((recipes) => {
+        Mealplan.find({ owner: req.params.id }).then((mealplans) => {
+          Profile.findById(req.user.profile._id).then((self) => {
+            const isSelf = self._id.equals(profile._id);
+            res.render("profiles/show", {
+              title: `${profile.name}'s profile`,
+              mealplans,
+              profile,
+              self,
+              isSelf,
+              recipes,
+            });
+          });
+        });
+      });
     })
-  .catch((err) => {
-    console.log(err)
-    res.redirect("/")
-  })
+    .catch((err) => {
+      console.log(err);
+      res.redirect("/");
+    });
 }
 
-export {
-  index,
-  show
-}
+export { index, show };
